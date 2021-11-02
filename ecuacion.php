248f4c7014649c0b8dde1b0e54be231e018dce79
MakaGrimhilde <simtoclatro@gmail.com>
2021-11-02 18:13:48 +0100

    Commit de ecuacion.php

---
+++ ecuacion.php
@@ -0,0 +1,39 @@
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+        <title>Ejercicio 1</title> 
+    </head>
+    <body>
+            <br/>
+            <div class="row justify-content-center">
+                <h2>Ejercicio 1</h2>
+            </div>
+            <br/>
+            <div class="row justify-content-center">
+                <form class="form-horizontal" method="POST" action="#">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Valor del coeficiente 1</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput2">Valor del coeficiente 2</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput2">Valor del coeficiente 3</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput2">Valor del coeficiente 3</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput3">
+                    </div><div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="submit" class="btn btn-primary" name="boton">Calcular</button>
+                    </div>
+                </form>
+            </div>        
+    </body>
+</html>
